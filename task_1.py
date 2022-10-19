69fba9d1326d5665650db35f6308106a5cbb0cb6
Lera Andreeva <valeraandreeva148@icloud.com>
2022-10-19 18:48:43 +0300

    Task 1

--- task_1.py
+++ task_1.py
@@ -1,1 +1,8 @@
 word_1 = "Hello"
+word_2 = "Human"
+user_name = input("Введи свое имя: ")
+user_surname = input('Введи свою фамилию: ')
+user_var = int(input('Введи любое число: '))
+user_var_2 = int(input("Введи второе любое число: "))
+print(f'{word_2} {word_1} {user_name} {user_surname} {user_var_2} {user_var}')
+
